e20eef1bd5a26814c8b5e6ac473af9df0b87d917
defowler2005 <129338316+defowler2005@users.noreply.github.com>
2023-12-04 22:14:35 +1030

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const prefix = 'DF.';
 
 const client = new Client({
   intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    GatewayIntentBits.Guilds
   ],
 });
 
