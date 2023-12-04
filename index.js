fd92a1cdf6c97771bd7394f689d1a80a0d16a424
defowler2005 <129338316+defowler2005@users.noreply.github.com>
2023-12-04 22:00:03 +1030

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 console.info('Hi, I am a console. I am cool!')
+const { Client, GatewayIntentBits, Intents } = require('discord.js');
+const token = '';
+const prefix = 'DF.';
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return; // Ignore messages from bots
+  if (!message.content.startsWith(prefix)) return; // Ignore messages that don't start with the prefix
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'ping') {
+    message.reply('Pong!');
+  } else if (command === 'hello') {
+    message.reply('Hello!');
+  }
+  // Add more commands as needed
+});
+
+client.login(token);
